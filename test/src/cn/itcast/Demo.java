77cfba30b67b8eb9312ec01f401d0531c31f0f09
李嘉图 <123@0ds.cn>
2019-10-17 22:02:49 +0800

    添加test到git

---
+++ test/src/cn/itcast/Demo.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+/**
+ * @Author:李嘉图
+ * @Date:2019/10/17 21:52
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+        System.out.println("test.....");
+    }
+}
